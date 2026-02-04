6c66f690be9af60e32efc299f785fd31e2377dbb
lookup7067-create <lookup7067@gmail.com>
2026-02-04 20:03:31 +0900

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false, // process.env.NODE_ENV === "development", // Enable in dev for testing
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withPWA(nextConfig);
